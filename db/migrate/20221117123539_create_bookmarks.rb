651706028d16cfbd86d12543e2d9283adbbb26b4
George Hill <georgeophill@outlook.com>
2022-11-17 15:08:19 +0000

    Models created

---
+++ db/migrate/20221117123539_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookmarks do |t|
+      t.string :movie
+      t.string :list
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
